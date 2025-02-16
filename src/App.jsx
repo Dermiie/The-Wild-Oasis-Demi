0b6cec344f5e6c065a888f9efbb21729609a36cd
dermiie <demioyeniyi@gmail.com>
2025-02-16 11:09:38 +0000

    Building the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AppLayout from './ui/AppLayout';
 import Account from './pages/Account';
 import Dashboard from './pages/Dashboard';
 import Cabins from './pages/Cabins';
@@ -15,16 +16,18 @@ function App() {
       <GlobalStyles></GlobalStyles>
       <BrowserRouter>
         <Routes>
-          <Route
-            index
-            element={<Navigate replace to={'dashboard'}></Navigate>}
-          />
-          <Route path="dashboard" element={<Dashboard></Dashboard>} />
-          <Route path="bookings" element={<Bookings></Bookings>} />
-          <Route path="cabins" element={<Cabins></Cabins>} />
-          <Route path="users" element={<Users></Users>} />
-          <Route path="settings" element={<Settings></Settings>} />
-          <Route path="account" element={<Account></Account>} />
+          <Route element={<AppLayout></AppLayout>}>
+            <Route
+              index
+              element={<Navigate replace to={'dashboard'}></Navigate>}
+            />
+            <Route path="dashboard" element={<Dashboard></Dashboard>} />
+            <Route path="bookings" element={<Bookings></Bookings>} />
+            <Route path="cabins" element={<Cabins></Cabins>} />
+            <Route path="users" element={<Users></Users>} />
+            <Route path="settings" element={<Settings></Settings>} />
+            <Route path="account" element={<Account></Account>} />
+          </Route>
           <Route path="login" element={<Login></Login>}></Route>
           <Route path="*" element={<PageNotFound></PageNotFound>} />
         </Routes>
