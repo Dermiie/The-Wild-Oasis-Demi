2e1aa5e7cda69ad41fce0dce7a0a7ff1a4965443
Dermiie <demioyeniyi@gmail.com>
2026-03-23 10:40:49 +0100

    feat- implemented the logout feature

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,20 @@
+import ButtonIcon from '../../ui/ButtonIcon';
+import {
+  HiArrowDownRight,
+  HiArrowRight,
+  HiArrowRightOnRectangle,
+} from 'react-icons/hi2';
+import { useLogout } from './useLogout';
+import SpinnerMini from '../../ui/SpinnerMini';
+
+function Logout() {
+  const { logout, isLoading } = useLogout();
+
+  return (
+    <ButtonIcon onClick={() => logout()}>
+      {isLoading ? <SpinnerMini /> : <HiArrowRightOnRectangle />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
--- src/features/authentication/useAuth.js
+++ src/features/authentication/useAuth.js
@@ -1,16 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { login as loginApi } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
 
 export function useAuth() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: (data) => {
-      navigate('/dashboard');
-      console.log('user', data);
+    onSuccess: (user) => {
+      navigate('/dashboard', { replace: true });
+      queryClient.setQueryData[('user', user)];
       toast.success('User logged in successfully');
     },
 
