90ce172e074a44f5ffacdd68e4f6eee2949ab4ed
Dermiie <demioyeniyi@gmail.com>
2026-04-06 17:39:54 +0100

    chore- completion of the dashboad analytics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,6 +34,8 @@ function DashboardLayout() {
         numDays={numOfDays}
         cabins={cabins}
       />
+      <TodayActivity />
+
       <DurationChart confirmedStays={stays} />
 
       <SalesChart bookings={bookings} numOfDays={numOfDays} />
